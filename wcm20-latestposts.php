d8b76daccc1acc1ad38c311d4d7320c6d0b0e2c8
Johan Nordström <johan@digitalvillage.se>
2021-05-24 11:55:08 +0200

    Working example of a shortcode that queries for the latest posts.

---
+++ wcm20-latestposts.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Plugin Name:	WCM20 Latest Posts
+ * Description:	This plugin adds a shortcode to display the latest posts
+ * Version:		0.1
+ * Author:		Johan Nordström
+ * Author URI:	https://www.thehiveresistance.com
+ * Text Domain:	wcm20-latestposts
+ * Domain Path:	/languages
+ */
+
+/**
+ * Register plugin shortcodes
+ */
+function wlp_init() {
+	// Register shortcode "latest-posts"
+	add_shortcode('latest-posts', 'wlp_shortcode_latest_posts');
+}
+add_action('init', 'wlp_init');
+
+/**
+ * Shortcode [latest-posts]
+ *
+ * @param array $args Arguments passed in shortcode
+ * @param mixed $content Content inside shortcode
+ * @return string
+ */
+function wlp_shortcode_latest_posts($args = [], $content = null) {
+	$output = "<h2>Latest Posts</h2>";
+
+	$posts = new WP_Query([
+		'posts_per_page' => 3,
+	]);
+
+	if ($posts->have_posts()) {
+		$output .= "<ul>";
+
+		// loop over available posts
+		while ($posts->have_posts()) {
+			$posts->the_post();
+
+			$output .= "<li>";
+			$output .= get_the_title();
+			$output .= "</li>";
+		}
+
+		// reset postdata
+		wp_reset_postdata();
+
+		$output .= "</ul>";
+
+	} else {
+		$output .= "<p><em>Sorry, no latest posts available.</em></p>";
+	}
+
+	return $output;
+}
